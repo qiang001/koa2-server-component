944a5b169d97d96b0498bc17782434802185e757
ZHOU QIANG <ntu.zhouqiang@gmail.com>
2019-09-13 14:53:57 +0800

    改进

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router')
-const v1 = new Router({prefix:'/v1/{{names}}'}) // 业务一级路由用复数
+const v1 = new Router({ prefix: '/v1/{{names}}' }) // 业务一级路由用复数
 const Controllers = require('./controllers')
 
-v1.get('/sayHello',Controllers.sayHello)
+v1.get('/sayHello', Controllers.sayHello)
 
-module.exports = v1
+module.exports = [v1]
