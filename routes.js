a6ad84fc374b6c1e6c3d13c85c8cb8b75cdca08b
ZHOU QIANG <ntu.zhouqiang@gmail.com>
2019-12-23 11:40:07 +0800

    添加api-url

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const Router = require('koa-router')
-const v1 = new Router({ prefix: '/v1/{{names}}' }) // 业务一级路由用复数
+const v1 = new Router({ prefix: '/api/v1/{{names}}' }) // 业务一级路由用复数
 const Controllers = require('./controllers')
 
 v1.get('/sayHello', Controllers.sayHello)
